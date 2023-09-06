4cfe6c783ccd5208595f7c66d014a083df36295d
kenagy100@gmail.com <kenagy100@gmail.com>
2023-09-06 00:28:10 +0000

    Modify class Person

--- classes/person.rb
+++ classes/person.rb
@@ -1,6 +1,6 @@
 class Person
   attr_accessor :name, :age
-  attr_reader: id
+  attr_reader :id
 
   def initialization (name = 'Unknow', age, parent_permission: true)
     @id = Random.rand(1..1000)
