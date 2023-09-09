9623274bc36655d182fa7fdd08936ef1ef8c125a
kenagy100@gmail.com <kenagy100@gmail.com>
2023-09-09 09:05:09 +0000

    Refactor code to reduce complexity

--- main.rb
+++ main.rb
@@ -1,52 +1,62 @@
 require './app'
 
-def main
-  app = App.new
+class LibraryApp
+  def initialize
+    @app = App.new
+  end
 
-  puts 'Welcome to the OOP School Library App'
-  puts
+  def run
+    puts 'Welcome to the OOP School Library App'
+    puts
 
-  loop do
-    case menu
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      puts 'Enter person\'s id'
-      person_id = gets.chomp.to_i
-      app.list_all_rentals(person_id)
-    when 7
-      puts 'Thank you for using this app!'
-      exit
-    else
-      puts 'Please choose a number between 1 and 7'
+    loop do
+      choice = display_menu
+
+      case choice
+      when 1 then @app.list_books
+      when 2 then @app.list_people
+      when 3 then @app.create_person
+      when 4 then @app.create_book
+      when 5 then @app.create_rental
+      when 6 then list_rentals_for_person
+      when 7 then exit_app
+      else
+        puts 'Please choose a number between 1 and 7'
+      end
     end
   end
-end
 
-def menu
-  puts 'Please select an option by entering its respective number'
-  list = {
-    1 => 'List all books',
-    2 => 'List all people',
-    3 => 'Create a person',
-    4 => 'Create a book',
-    5 => 'Create a rental',
-    6 => 'List all rentals for a given person id',
-    7 => 'Exit'
-  }
+  private
 
-  list.each do |index, string|
-    puts "#{index} - #{string}"
+  def display_menu
+    puts 'Please select an option by entering its respective number'
+    menu_options = {
+      1 => 'List all books',
+      2 => 'List all people',
+      3 => 'Create a person',
+      4 => 'Create a book',
+      5 => 'Create a rental',
+      6 => 'List all rentals for a given person id',
+      7 => 'Exit'
+    }
+
+    menu_options.each do |index, string|
+      puts "#{index} - #{string}"
+    end
+
+    gets.chomp.to_i
   end
-  gets.chomp.to_i
+
+  def list_rentals_for_person
+    puts 'Enter person\'s id'
+    person_id = gets.chomp.to_i
+    @app.list_all_rentals(person_id)
+  end
+
+  def exit_app
+    puts 'Thank you for using this app!'
+    exit
+  end
 end
 
-main
+LibraryApp.new.run
