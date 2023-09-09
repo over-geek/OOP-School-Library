13e7ee97acd457dd22150629a22338e0356cc733
kenagy100@gmail.com <kenagy100@gmail.com>
2023-09-09 09:20:36 +0000

    Refactor code to reduce cyclomatic complexity

--- main.rb
+++ main.rb
@@ -1,50 +1,75 @@
 require './app'
 
 class LibraryApp
+  MENU_OPTIONS = {
+    1 => { text: 'List all books', action: :list_books },
+    2 => { text: 'List all people', action: :list_people },
+    3 => { text: 'Create a person', action: :create_person },
+    4 => { text: 'Create a book', action: :create_book },
+    5 => { text: 'Create a rental', action: :create_rental },
+    6 => { text: 'List all rentals for a given person id', action: :list_rentals_for_person },
+    7 => { text: 'Exit', action: :exit_app }
+  }.freeze
+
   def initialize
     @app = App.new
   end
 
   def run
-    puts 'Welcome to the OOP School Library App'
-    puts
+    display_welcome_message
 
     loop do
       choice = display_menu
-
-      case choice
-      when 1 then @app.list_books
-      when 2 then @app.list_people
-      when 3 then @app.create_person
-      when 4 then @app.create_book
-      when 5 then @app.create_rental
-      when 6 then list_rentals_for_person
-      when 7 then exit_app
-      else
-        puts 'Please choose a number between 1 and 7'
-      end
+      process_choice(choice)
     end
   end
 
   private
 
+  def display_welcome_message
+    puts 'Welcome to the OOP School Library App'
+    puts
+  end
+
   def display_menu
     puts 'Please select an option by entering its respective number'
-    menu_options = {
-      1 => 'List all books',
-      2 => 'List all people',
-      3 => 'Create a person',
-      4 => 'Create a book',
-      5 => 'Create a rental',
-      6 => 'List all rentals for a given person id',
-      7 => 'Exit'
-    }
-
-    menu_options.each do |index, string|
-      puts "#{index} - #{string}"
-    end
-
+    list_menu_options
     gets.chomp.to_i
+  end
+
+  def list_menu_options
+    MENU_OPTIONS.each do |index, option|
+      puts "#{index} - #{option[:text]}"
+    end
+  end
+
+  def process_choice(choice)
+    selected_option = MENU_OPTIONS[choice]
+    if selected_option
+      send(selected_option[:action])
+    else
+      puts 'Please choose a number between 1 and 7'
+    end
+  end
+
+  def list_books
+    @app.list_books
+  end
+
+  def list_people
+    @app.list_people
+  end
+
+  def create_person
+    @app.create_person
+  end
+
+  def create_book
+    @app.create_book
+  end
+
+  def create_rental
+    @app.create_rental
   end
 
   def list_rentals_for_person
